d89d40970b6dec59e3909d0232a069105c66439b
Naomi Dushay <ndushay@stanford.edu>
2017-05-05 15:38:20 -0700

    populate settings object by reading config/settings.properties file

--- src/edu/stanford/dlss/was/WasapiDownloader.java
+++ src/edu/stanford/dlss/was/WasapiDownloader.java
@@ -1,8 +1,49 @@
 package edu.stanford.dlss.was;
 
+import java.io.*;
+import java.util.Properties;
+
 public class WasapiDownloader
 {
-    public static void main(String[] args) {
-        System.out.println("I am WasapiDownloader. Hear me roar. Woo.");
+  public static final String SETTINGS_FILE_LOCATION = "config/settings.properties";
+  public Properties settings = null;
+
+  public WasapiDownloader() {
+    loadPropertiesFile();
+  }
+
+  private void loadPropertiesFile() {
+    if (settings == null)
+    {
+      InputStream input = null;
+      try {
+        input = new FileInputStream(SETTINGS_FILE_LOCATION);
+        settings = new Properties();
+        settings.load(input);
+      } catch (IOException e) {
+        e.printStackTrace();
+        System.exit(99);
+      }  finally {
+        if (input != null) {
+          try {
+            input.close();
+          } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(99);
+          }
+        }
+      }
     }
+    else
+    {
+      System.err.println("Properties already loaded from " + SETTINGS_FILE_LOCATION);
+    }
+  }
+
+
+  public static void main(String[] args) {
+    System.out.println("I am WasapiDownloader. Hear me roar. Woo.");
+    new WasapiDownloader();
+  }
+
 }
--- test/edu/stanford/dlss/was/TestWasapiDownloader.java
+++ test/edu/stanford/dlss/was/TestWasapiDownloader.java
@@ -1,6 +1,7 @@
 package edu.stanford.dlss.was;
 
 import static org.junit.Assert.*;
+
 import org.junit.*;
 
 import edu.stanford.dlss.was.WasapiDownloader;
@@ -11,6 +12,16 @@ public class TestWasapiDownloader
     @Before
     public void setUp()
     {
+    }
+
+    @Test
+    public void constructorReadsPropertiesFile()
+    {
+      WasapiDownloader myInstance = new WasapiDownloader();
+      assertNotNull(myInstance.settings);
+      assertEquals("incorrect default settings.properties baseurl value", myInstance.settings.getProperty("baseurl"), "http://example.org");
+      assertEquals("incorrect default settings.properties username value", myInstance.settings.getProperty("username"), "user");
+      assertEquals("incorrect default settings.properties password value", myInstance.settings.getProperty("password"), "pass");
     }
 
     @Test
