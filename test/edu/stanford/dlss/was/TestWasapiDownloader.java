0dc5c97577d611fe86d4d99361a9d1d4558b8e50
Naomi Dushay <ndushay@stanford.edu>
2017-05-05 15:38:20 -0700

    populate settings object by reading config/settings.properties file

--- test/edu/stanford/dlss/was/TestWasapiDownloader.java
+++ test/edu/stanford/dlss/was/TestWasapiDownloader.java
@@ -1,21 +1,29 @@
 package edu.stanford.dlss.was;
 
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 import org.junit.*;
 
 import edu.stanford.dlss.was.WasapiDownloader;
 
-public class TestWasapiDownloader
-{
+public class TestWasapiDownloader {
 
     @Before
-    public void setUp()
-    {
+    public void setUp() {
     }
 
     @Test
-    public void canExecuteMainWithoutCrashing()
-    {
-        WasapiDownloader.main(null);
+    public void constructorReadsPropertiesFile() throws IOException {
+      WasapiDownloader myInstance = new WasapiDownloader();
+      assertNotNull(myInstance.settings);
+      assertEquals("incorrect default settings.properties baseurl value", myInstance.settings.getProperty("baseurl"), "http://example.org");
+      assertEquals("incorrect default settings.properties username value", myInstance.settings.getProperty("username"), "user");
+      assertEquals("incorrect default settings.properties password value", myInstance.settings.getProperty("password"), "pass");
+    }
+
+    @Test
+    public void canExecuteMainWithoutCrashing() throws IOException {
+      WasapiDownloader.main(null);
     }
 }
