6ed1ec47dac140695bfb6db2d5b6ca3e862e5725
Naomi Dushay <ndushay@stanford.edu>
2017-05-23 14:51:36 -0700

    TestWasapiConnection - remove some unused stuff

--- test/edu/stanford/dlss/was/TestWasapiConnection.java
+++ test/edu/stanford/dlss/was/TestWasapiConnection.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 
 import org.apache.http.client.methods.HttpGet;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
 import org.junit.*;
 import static org.mockito.Mockito.*;
 import org.mockito.ArgumentMatchers;
@@ -13,13 +11,13 @@ import org.mockito.ArgumentMatchers;
 public class TestWasapiConnection {
 
   private static final String JSON_QUERY = "http://example.com/example.json";
-  private static final String DOWNLOAD_QUERY = "http://example.com/download?file=example.warc";
+  // private static final String DOWNLOAD_QUERY = "http://example.com/download?file=example.warc";
   private static final String OUTPUT_PATH = "/dev/null";
 
   @Test
   public void constructorCallsLogin() throws IOException {
     WasapiClient mockClient = mock(WasapiClient.class);
-    WasapiConnection testConnection = new WasapiConnection(mockClient);
+    new WasapiConnection(mockClient);
 
     verify(mockClient, times(1)).login();
   }
